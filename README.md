16bd95a20e352c909704befcd2593caffdff4ac9
Wang, Yue A <yue.a.wang@intel.com>
2018-02-27 10:31:51 +0800

    Update script, conf and readme.
    Signed-off-by: Wang, Yue A <yue.a.wang@intel.com>

--- README.md
+++ README.md
@@ -5,10 +5,15 @@ OAP Cluster Performance TestSuite
 Use "sbt assembly" because it refers to org.reflections which may not exist in cluster env.
 
 ## Test
-For cluster test:
-OapBenchmark -c Config (oap/parquet, index/non-index, etc.) -s SuiteName (All, etc.) -t TestName (All, etc.) -r Repeat(3) 
+### For cluster test:
 
-For self test or local debug purpose:
+#### GenData:
+OapBenchmark -d
+
+#### Run Tests:
+OapBenchmark -c Config (oap/parquet, index/non-index, etc.) -s SuiteName (All, etc.) -t TestName (All, etc.) -r Repeat(3)
+
+### For self test or local debug purpose:
 OapBenchmark -bootstrapping
 
-## TODO: Add Suite
+## TODO: Add More Explanations.
--- bin/run.sh
+++ bin/run.sh
@@ -2,9 +2,16 @@
 
 OAP_PERF_SUITE_HOME=/home/oap/works/oap-perf-suite/
 SPARK_HOME=/home/oap/spark/spark-2.1.0-bin-hadoop2.7
+#TODO: add check
 cd $SPARK_HOME
 
+#Gen Data If this is the first time of running or whenever you want.
 #sudo bin/spark-submit --master yarn --deploy-mode client --class org.apache.spark.sql.OapPerfSuite $OAP_PERF_SUITE_HOME/target/scala-2.11/oap-perf-assembly-1.0.jar -d
+
+#Run whole benchmark with all testcases in all testsuites.
+#sudo bin/spark-submit --master yarn --deploy-mode client --class org.apache.spark.sql.OapPerfSuite $OAP_PERF_SUITE_HOME/target/scala-2.11/oap-perf-assembly-1.0.jar
+
+#Run different test according to different settings.
 #sudo bin/spark-submit --master yarn --deploy-mode client --class org.apache.spark.sql.OapPerfSuite $OAP_PERF_SUITE_HOME/target/scala-2.11/oap-perf-assembly-1.0.jar -r 3
 #sudo bin/spark-submit --master yarn --deploy-mode client --class org.apache.spark.sql.OapPerfSuite $OAP_PERF_SUITE_HOME/target/scala-2.11/oap-perf-assembly-1.0.jar -s OapStrategySuite
 #sudo bin/spark-submit --master yarn --deploy-mode client --class org.apache.spark.sql.OapPerfSuite $OAP_PERF_SUITE_HOME/target/scala-2.11/oap-perf-assembly-1.0.jar -t "Limit 10 from whole table"
