ddc94230c1552b4d473e4a555f7f221aea2edbb6
Wang, Yue A <yue.a.wang@intel.com>
2018-04-08 15:32:25 +0800

    Fix 1 issues:
    Drop table before new table generation.
    
    Signed-off-by: Wang, Yue A <yue.a.wang@intel.com>

--- src/main/scala/org/apache/spark/sql/OapBenchmarkDataBuilder.scala
+++ src/main/scala/org/apache/spark/sql/OapBenchmarkDataBuilder.scala
@@ -79,8 +79,8 @@ object OapBenchmarkDataBuilder extends OapPerfSuiteContext with Logging {
     val hdfsRootDir = properties.get("oap.benchmark.hdfs.file.root.dir").get
     val tpcdsToolPath = properties.get("oap.benchmark.tpcds.tool.dir").get
 
-    sqlContext.setConf("spark.sql.parquet.compression.codec", codec)
     dataFormats.foreach{ format =>
+      sqlContext.setConf(s"spark.sql.$format.compression.codec", codec)
       val loc = formatTableLocation(hdfsRootDir, versionNum, format)
       val tables = new Tables(sqlContext, tpcdsToolPath, scale)
       tables.genData(
